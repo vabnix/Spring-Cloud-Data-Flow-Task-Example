d5a172d6243ec993cff51307171aa3df2f04a70f
Vaibhav Srivastava <vsrivastava@lwolf.com>
2021-01-05 10:08:21 -0600

    Initial code for Spring Cloud Data flow task

---
+++ src/main/java/com/vaibhav/scdf/task/SpringCloudDataFlowTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.scdf.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDataFlowTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDataFlowTaskApplication.class, args);
+    }
+
+}
